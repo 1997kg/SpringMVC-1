f79b226ec01fbab5f5b2c8e785bb9fbde38e1f91
1997kg <dojug3@naver.com>
2024-11-01 04:59:14 +0900

    HTTP- GET query parameter

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -14,6 +14,7 @@ import java.util.Enumeration;
  * http://localhost:8080/request-param?username=hello&age=20
  *
  *
+ *
  */
 
 @WebServlet(name =  "requestParamServlet", urlPatterns = "/request-param")
