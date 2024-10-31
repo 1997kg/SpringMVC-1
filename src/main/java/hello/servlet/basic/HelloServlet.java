3533d0b6ad0b50304366f6549fc411907ff7c0c9
1997kg <dojug3@naver.com>
2024-11-01 04:26:52 +0900

    HttpServlet-basic,outline,inline

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,30 @@
+package hello.servlet.basic;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override //service 메서드 호출
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+
+    }
+}
