aa30004206239cb8169002b44705efc428a4939e
1997kg <dojug3@naver.com>
2024-11-05 10:59:34 +0900

    ControllerV2(MyView)

--- src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -19,25 +19,24 @@ public class FrontControllerServletV1 extends HttpServlet {
     private Map<String, ControllerV1> controllerMap = new HashMap<>();
 
     public FrontControllerServletV1() {
-        controllerMap.put("/front-controller/v1/members/new-from", new MemberFormControllerV1());
+        controllerMap.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
         controllerMap.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
         controllerMap.put("/front-controller/v1/members", new MemberListControllerV1());
-
     }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("FrontControllerServletV1 service");
+        System.out.println("FrontControllerServletV1.service");
 
-        // /front-controller/v1/members
-        String requestURl = request.getRequestURI();
+        // front-controller/v1/members
+        String requestURI = request.getRequestURI();
 
-        ControllerV1 controller = controllerMap.get(requestURl);
+        ControllerV1 controller = controllerMap.get(requestURI);
         if (controller == null) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
 
-        controller.process(request,response);
+        controller.process(request, response);
     }
 }
